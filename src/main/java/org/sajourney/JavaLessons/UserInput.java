c0f5a4c7cf4c621143d9587f38816dc39c33bff7
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-02 12:53:47 +0300

    User inputs

---
+++ src/main/java/org/sajourney/JavaLessons/UserInput.java
@@ -0,0 +1,30 @@
+package org.sajourney.JavaLessons;
+
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String firstName = scan.next();
+        scan.nextLine();
+
+        System.out.println("Enter your names");
+        String fullNames = scan.nextLine();
+
+        System.out.println("Enter your age");
+        int age = scan.nextInt();
+
+        System.out.println("Enter value of pie");
+        float pie = scan.nextFloat();
+
+        System.out.println(firstName);
+        System.out.println(fullNames);
+        System.out.println(age);
+        System.out.println(pie);
+
+
+
+
+    }
+}
