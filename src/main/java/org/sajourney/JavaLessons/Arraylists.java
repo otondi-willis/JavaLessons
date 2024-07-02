b1e7057b23750b0c69d59fd53ccf6437afd95b79
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-02 22:53:06 +0300

    ArrayList of ArrayList 2D

--- src/main/java/org/sajourney/JavaLessons/Arraylists.java
+++ src/main/java/org/sajourney/JavaLessons/Arraylists.java
@@ -18,7 +18,37 @@ public class Arraylists {
         food.clear();
 
         for (int i=0; i<food.size(); i++){
-            System.out.println(food.get(i));
+            //System.out.println(food.get(i));
         }
+
+        // 2D ArrayList = dynamic list of lists
+        // you can change the size of these lists during runtime
+
+        ArrayList<ArrayList<String>> groceryList = new ArrayList();
+        ArrayList<String> bakeryList = new ArrayList<String>();
+        bakeryList.add("pasta");
+        bakeryList.add("garlic bread");
+        bakeryList.add("donuts");
+
+        ArrayList<String> produceList = new ArrayList<String>();
+        produceList.add("tomatoes");
+        produceList.add("zucchini");
+        produceList.add("peppers");
+
+        ArrayList<String> drinkList = new ArrayList<String>();
+        drinkList.add("krest");
+        drinkList.add("fanta");
+        drinkList.add("stoney");
+        drinkList.add("sprite");
+
+        groceryList.add(bakeryList);
+        groceryList.add(produceList);
+        groceryList.add(drinkList);
+
+
+        System.out.println(groceryList.get(0).get(1));
+
+
+
     }
 }
