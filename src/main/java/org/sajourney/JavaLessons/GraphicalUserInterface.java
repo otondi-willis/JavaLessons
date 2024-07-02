f567a972aa5a50b79c6da6894f11d8ea93970002
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-02 13:05:19 +0300

    Added age dialog box

--- src/main/java/org/sajourney/JavaLessons/GraphicalUserInterface.java
+++ src/main/java/org/sajourney/JavaLessons/GraphicalUserInterface.java
@@ -7,6 +7,11 @@ public class GraphicalUserInterface {
         String name = JOptionPane.showInputDialog("Enter your name");
         JOptionPane.showMessageDialog(null,"Hello " + name);
 
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You are " + age + "years old");
+
+
+
 
     }
 }
