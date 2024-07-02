6a92494e4d70ac1557e924d31dff5ebf128c816c
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-02 22:28:58 +0300

    Wrapper classes

---
+++ src/main/java/org/sajourney/JavaLessons/WrapperClasses.java
@@ -0,0 +1,23 @@
+package org.sajourney.JavaLessons;
+
+public class WrapperClasses {
+    public static void main(String[] args){
+        //primitive data types to reference data types
+        //autoboxing = automatic conversion primitive to object wrapper class
+        // unboxing = reverse of autoboxing (automatic)
+
+        //autoboxing
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double e = 3.14;
+
+        //unboxing
+        if (a == true){
+            System.out.println("true");
+        }
+
+
+
+    }
+}
