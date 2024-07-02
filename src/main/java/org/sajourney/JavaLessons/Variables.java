9c825a9e9bef926adf1fd2c29d654c0cafd36cca
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-02 12:19:16 +0300

    first commit on java lessons

---
+++ src/main/java/org/sajourney/JavaLessons/Variables.java
@@ -0,0 +1,23 @@
+package org.sajourney.JavaLessons;
+
+public class Variables {
+    public static void main (String[] args){
+        //primitives
+        boolean isBoolean = true;
+        byte byteNum = 127;
+        long longInt = 2000000000;
+        int justInt = 20;
+        short shortInt= 20;
+        char character = 'A';
+        float floatingPointValues = 345.6789f;
+        double bigFloatingPointValues = 345.789790;
+
+        //reference
+        String name = "Willis Otondi";
+
+
+
+
+
+    }
+}
