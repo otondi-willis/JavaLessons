000e137f27418a393294a772a0d1dcba8a10a1c5
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-02 13:01:08 +0300

    Added message dialog box

--- src/main/java/org/sajourney/JavaLessons/GraphicalUserInterface.java
+++ src/main/java/org/sajourney/JavaLessons/GraphicalUserInterface.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 public class GraphicalUserInterface {
     public static void main(String[] args){
         String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null,"Hello " + name);
+
 
     }
 }
