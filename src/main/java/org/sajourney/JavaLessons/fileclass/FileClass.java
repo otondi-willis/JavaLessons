a7789cb5cae449b839ba9d4048d66f27b8e98fef
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-03 19:41:37 +0300

    Finding files

---
+++ src/main/java/org/sajourney/JavaLessons/fileclass/FileClass.java
@@ -0,0 +1,17 @@
+package org.sajourney.JavaLessons.fileclass;
+
+import java.io.File;
+
+public class FileClass {
+    // file = an abstract representation of file and directory pathnames
+    public static void main(String[] args){
+        File file = new File("D:\\Projects\\JavaLessons\\src\\main\\java\\org\\sajourney\\JavaLessons\\fileclass\\analytics.txt");
+        if(file.exists()){
+            System.out.println("That file exists");
+        }
+        else {
+            System.out.println("That file doesnot exist");
+        }
+    }
+
+}
