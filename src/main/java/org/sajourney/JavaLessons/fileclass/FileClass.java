27ded774a2100664d115893a85f8defcb51fe41c
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-03 19:46:48 +0300

    getting file path and deleting file

--- src/main/java/org/sajourney/JavaLessons/fileclass/FileClass.java
+++ src/main/java/org/sajourney/JavaLessons/fileclass/FileClass.java
@@ -8,6 +8,10 @@ public class FileClass {
         File file = new File("D:\\Projects\\JavaLessons\\src\\main\\java\\org\\sajourney\\JavaLessons\\fileclass\\analytics.txt");
         if(file.exists()){
             System.out.println("That file exists");
+            System.out.println(file.getPath());
+            System.out.println(file.getAbsolutePath());
+            System.out.println(file.isFile());
+            file.delete();
         }
         else {
             System.out.println("That file doesnot exist");
