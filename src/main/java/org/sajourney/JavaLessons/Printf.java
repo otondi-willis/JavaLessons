b774f9412a5d28e1a26cf073610e0e58151d1779
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-03 08:59:42 +0300

    printf flags

--- src/main/java/org/sajourney/JavaLessons/Printf.java
+++ src/main/java/org/sajourney/JavaLessons/Printf.java
@@ -14,17 +14,30 @@ public class Printf {
         double myDouble = 1000;
 
         //conversion characters
-        System.out.printf("%b", myBoolean);
-        System.out.printf("%c", myChar);
-        System.out.printf("%s", myString);
-        System.out.printf("%d", myInt);
-        System.out.printf("%f", myDouble);
-        System.out.printf("This is a format string %d", 123);
+        System.out.printf("\n %b", myBoolean);
+        System.out.printf("\n %c", myChar);
+        System.out.printf("\n %s", myString);
+        System.out.printf("\n %d", myInt);
+        System.out.printf("\n %f", myDouble);
+        System.out.printf("\n This is a format string %d", 123);
 
         //width
         System.out.printf("\n Hello %10s", myString);
 
         //precision
-        System.out.printf("You have this much money %.2f",myDouble);
+        System.out.printf("\n You have this much money %.2f",myDouble);
+
+        //flags - adds an effect to output based on the flag added to format specifier
+        //- : left-justify
+        //+ : output a plus (+) or minus (-) sign for a numeric value
+        //0 : numeric values are zero-padded
+        //, : comma grouping separator if numbers > 1000
+        System.out.printf("\n You have this much money %20f",myDouble);
+        System.out.printf("\n You have this much money %020f",myDouble);
+        System.out.printf("\n You have this much money %+20f",myDouble);
+        System.out.printf("\n You have this much money %-20f",myDouble);
+        System.out.printf("\n You have this much money %,20f",myDouble);
+
+
     }
 }
