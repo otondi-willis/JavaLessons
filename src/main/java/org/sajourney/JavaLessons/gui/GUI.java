fd457a20b9da7950d0fc6e65c49eee37ebbb594b
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-04 10:00:34 +0300

    Removed resizable option

--- src/main/java/org/sajourney/JavaLessons/gui/GUI.java
+++ src/main/java/org/sajourney/JavaLessons/gui/GUI.java
@@ -8,6 +8,7 @@ public class GUI {
         JFrame frame = new JFrame();// creates a frame
         frame.setTitle("Title goes here");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
         frame.setSize(420,420);// sets the x-dimension and y- dimension
         frame.setVisible(true);// makes frame visible
     }
