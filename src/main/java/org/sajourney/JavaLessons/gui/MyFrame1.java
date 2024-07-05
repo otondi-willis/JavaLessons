60b75c5fbbc038bc09765de243d92ef896577b2f
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-05 22:17:52 +0300

    Added action listener

--- src/main/java/org/sajourney/JavaLessons/gui/MyFrame1.java
+++ src/main/java/org/sajourney/JavaLessons/gui/MyFrame1.java
@@ -1,16 +1,28 @@
 package org.sajourney.JavaLessons.gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFrame1 extends JFrame {
+public class MyFrame1 extends JFrame implements ActionListener {
+    JButton button;
     MyFrame1(){
-        JButton button = new JButton();
+        button = new JButton();
         button.setBounds(200,100,100,50);
+        button.addActionListener(this);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500,500);
         this.setVisible(true);
         this.add(button);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==button){
+            System.out.println("poo");
+
+        }
     }
 }
