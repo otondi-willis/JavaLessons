f383205aaf1327ca6edd4691ed1c1138fca87af4
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-05 21:30:51 +0300

    Created and added a frame

---
+++ src/main/java/org/sajourney/JavaLessons/gui/Buttons.java
@@ -0,0 +1,9 @@
+package org.sajourney.JavaLessons.gui;
+
+public class Buttons {
+    public static void main(String[] args){
+        // JButton = a button that performs an action when clicked on
+        new MyFrame1();
+
+    }
+}
---
+++ src/main/java/org/sajourney/JavaLessons/gui/MyFrame1.java
@@ -0,0 +1,12 @@
+package org.sajourney.JavaLessons.gui;
+
+import javax.swing.*;
+
+public class MyFrame1 extends JFrame {
+    MyFrame1(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500,500);
+        this.setVisible(true);
+    }
+}
