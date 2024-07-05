0d1727f70d0b372c2c077fb87ed47617f1c91cc8
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-05 22:11:52 +0300

    Added a button

--- src/main/java/org/sajourney/JavaLessons/gui/MyFrame1.java
+++ src/main/java/org/sajourney/JavaLessons/gui/MyFrame1.java
@@ -4,9 +4,13 @@ import javax.swing.*;
 
 public class MyFrame1 extends JFrame {
     MyFrame1(){
+        JButton button = new JButton();
+        button.setBounds(200,100,100,50);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500,500);
         this.setVisible(true);
+        this.add(button);
     }
 }
