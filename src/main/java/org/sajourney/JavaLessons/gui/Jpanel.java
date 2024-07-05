743486b17f383a752eccf56d45d9d882376cd828
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-05 21:03:25 +0300

    Added blue panel to the frame

--- src/main/java/org/sajourney/JavaLessons/gui/Jpanel.java
+++ src/main/java/org/sajourney/JavaLessons/gui/Jpanel.java
@@ -9,12 +9,18 @@ public class Jpanel {
         JPanel redPanel = new JPanel();
         redPanel.setBackground(Color.red);
         redPanel.setBounds(0,0,250,250);
+
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(250,0,250,250);
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setSize(750, 750);
         frame.setVisible(true);
         frame.add(redPanel);
+        frame.add(bluePanel);
     }
 
 
