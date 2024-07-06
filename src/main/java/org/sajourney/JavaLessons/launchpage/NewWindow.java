5b758ead585fde82af9a13a6ad84cdab27439b5b
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-06 16:54:40 +0300

    created launch window

--- src/main/java/org/sajourney/JavaLessons/launchpage/NewWindow.java
+++ src/main/java/org/sajourney/JavaLessons/launchpage/NewWindow.java
@@ -1,4 +1,23 @@
 package org.sajourney.JavaLessons.launchpage;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class NewWindow {
+
+    JFrame frame = new JFrame();
+    JLabel label = new JLabel("Hello");
+
+    NewWindow(){
+        label.setBounds(0,0,100,50);
+        label.setFont(new Font(null,Font.PLAIN,25));
+
+        frame.add(label);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,420);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
+
 }
