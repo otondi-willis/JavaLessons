dc75bf70cd2b736f49245ac690b6d89f18854487
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-06 15:30:18 +0300

    Added leading flow layout

--- src/main/java/org/sajourney/JavaLessons/gui/FlowLyout.java
+++ src/main/java/org/sajourney/JavaLessons/gui/FlowLyout.java
@@ -16,7 +16,7 @@ public class FlowLyout {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
-        frame.setLayout(new FlowLayout());
+        frame.setLayout(new FlowLayout(FlowLayout.LEADING));
 
 
         frame.add(new JButton("1"));
