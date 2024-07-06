5e83e7b09e0be258cfb5c9212986cdf4c98a523a
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-06 16:26:32 +0300

    Added labels to the layered panes

--- src/main/java/org/sajourney/JavaLessons/gui/LayredPane.java
+++ src/main/java/org/sajourney/JavaLessons/gui/LayredPane.java
@@ -9,9 +9,28 @@ public class LayredPane {
 
     public static void main(String[] args){
 
+        JLabel label1 = new JLabel();
+        label1.setOpaque(true);
+        label1.setBackground(Color.RED);
+        label1.setBounds(50,50,200,200);
+
+        JLabel label2 = new JLabel();
+        label2.setOpaque(true);
+        label2.setBackground(Color.green);
+        label2.setBounds(100,100,200,200);
+
+        JLabel label3 = new JLabel();
+        label3.setOpaque(true);
+        label3.setBackground(Color.orange);
+        label3.setBounds(150,150,200,200);
+
+
+
         JLayeredPane layeredPane = new JLayeredPane();
         layeredPane.setBounds(0,0,500,500);
+
         JFrame frame = new JFrame("JLayeredPane");
+        frame.add(layeredPane);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(new Dimension(500,500));
         frame.setLayout(null);
