5be397498a73ebdf53185718662c25bfee180d50
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-07 16:19:12 +0300

    yes no confirmation dialogue box

--- src/main/java/org/sajourney/JavaLessons/gui/OptionPane.java
+++ src/main/java/org/sajourney/JavaLessons/gui/OptionPane.java
@@ -7,11 +7,14 @@ public class OptionPane {
         //JOptionPane
         //pop up standard dialogue box that prompts users for a value or informs them sth
 
-        JOptionPane.showMessageDialog(null,"Hello there","title",JOptionPane.PLAIN_MESSAGE);
-        JOptionPane.showMessageDialog(null,"Hello there","title",JOptionPane.INFORMATION_MESSAGE);
-        JOptionPane.showMessageDialog(null,"Hello there","title",JOptionPane.QUESTION_MESSAGE);
-        JOptionPane.showMessageDialog(null,"Hello there","title",JOptionPane.WARNING_MESSAGE);
-        JOptionPane.showMessageDialog(null,"Hello there","title",JOptionPane.ERROR_MESSAGE);
+//        JOptionPane.showMessageDialog(null,"Plain message","title",JOptionPane.PLAIN_MESSAGE);
+//        JOptionPane.showMessageDialog(null,"Information message","title",JOptionPane.INFORMATION_MESSAGE);
+//        JOptionPane.showMessageDialog(null,"Question message?","title",JOptionPane.QUESTION_MESSAGE);
+//        JOptionPane.showMessageDialog(null,"Warning message","title",JOptionPane.WARNING_MESSAGE);
+//        JOptionPane.showMessageDialog(null,"Error message","title",JOptionPane.ERROR_MESSAGE);
+
+
+        JOptionPane.showConfirmDialog(null,"Do you even code?","This is my title", JOptionPane.YES_NO_OPTION);
 
 
 
