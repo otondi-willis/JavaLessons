9ea9ea76cf8dac9154cde2a9812a5bc50668f1aa
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-07 16:38:23 +0300

    initial textfield component commit

---
+++ src/main/java/org/sajourney/JavaLessons/gui/TextFld.java
@@ -0,0 +1,10 @@
+package org.sajourney.JavaLessons.gui;
+
+public class TextFld {
+    public static void main(String[] args){
+        // JTextField
+        // A GUI textbox component that can be used to add, set or get text
+
+
+    }
+}
