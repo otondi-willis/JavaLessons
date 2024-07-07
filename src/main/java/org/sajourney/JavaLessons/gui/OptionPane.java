05f6ca71da062bfe4bf92a7fb23e8b55ac2a3c58
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-07 16:09:35 +0300

    Created standard dialogue box

---
+++ src/main/java/org/sajourney/JavaLessons/gui/OptionPane.java
@@ -0,0 +1,14 @@
+package org.sajourney.JavaLessons.gui;
+
+import javax.swing.*;
+
+public class OptionPane {
+    public static void main(String[] args){
+        //JOptionPane
+        //pop up standard dialogue box that prompts users for a value or informs them sth
+
+        JOptionPane.showMessageDialog(null,"Hello there","title",JOptionPane.PLAIN_MESSAGE);
+
+
+    }
+}
