455820c23accacd86ad0a41cc6e6fd18db1a156b
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-07 17:18:53 +0300

    CheckInitial commit

---
+++ src/main/java/org/sajourney/JavaLessons/gui/CheckBox.java
@@ -0,0 +1,10 @@
+package org.sajourney.JavaLessons.gui;
+
+public class CheckBox {
+    public static void main (String[] args){
+        //JCheckBox
+        // can be selected or deselected
+
+
+    }
+}
