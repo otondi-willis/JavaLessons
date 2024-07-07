a903a7c0ab7b64fff228dca33dc6afcf98288445
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-07 17:44:32 +0300

    Added checkbox icons

--- src/main/java/org/sajourney/JavaLessons/checkbox/MyFrame.java
+++ src/main/java/org/sajourney/JavaLessons/checkbox/MyFrame.java
@@ -8,10 +8,15 @@ import java.awt.event.ActionListener;
 public class MyFrame extends JFrame implements ActionListener {
     JButton button;
     JCheckBox checkBox;
+    ImageIcon xIcon;
+    ImageIcon checkIcon;
 
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
+
+        xIcon = new ImageIcon("D:\\HT\\Teleafia_patient_app\\assets\\about_us.PNG");
+        checkIcon = new ImageIcon("D:\\HT\\Teleafia_patient_app\\assets\\bmi.PNG");
 
         button = new JButton();
         button.setText("Submit");
@@ -19,6 +24,9 @@ public class MyFrame extends JFrame implements ActionListener {
 
         checkBox = new JCheckBox();
         checkBox.setText("I am not a robot");
+        checkBox.setIcon(xIcon);
+        checkBox.setSelectedIcon(checkIcon);
+
 
         this.add(button);
         this.add(checkBox);
