d0271d32dbc8e5a542ea6615bcf4da97b1b1a86a
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-07 17:04:10 +0300

    Linked the button with action listener

--- src/main/java/org/sajourney/JavaLessons/textfield/MyFrame.java
+++ src/main/java/org/sajourney/JavaLessons/textfield/MyFrame.java
@@ -6,16 +6,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame implements ActionListener {
+    JButton button;
+    JTextField textField;
 
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
-        JButton button = new JButton("Submit");
+        button = new JButton("Submit");
+        button.addActionListener(this);
 
 
 
-        JTextField textField = new JTextField();
+        textField = new JTextField();
         textField.setPreferredSize(new Dimension(250, 40));
         this.add(button);
         this.add(textField);
@@ -24,6 +27,10 @@ public class MyFrame extends JFrame implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==button){
+            System.out.println("Welcome "+ textField.getText());
+
+        }
 
     }
 }
