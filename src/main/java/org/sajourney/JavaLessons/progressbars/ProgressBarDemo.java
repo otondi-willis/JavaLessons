11a0946c45ce8ae18187cad3d859a5caf8638555
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-08 09:35:51 +0300

    use of setStringPainted() method

--- src/main/java/org/sajourney/JavaLessons/progressbars/ProgressBarDemo.java
+++ src/main/java/org/sajourney/JavaLessons/progressbars/ProgressBarDemo.java
@@ -10,6 +10,7 @@ public class ProgressBarDemo {
 
         bar.setValue(0);
         bar.setBounds(0,0,420,50);
+        bar.setStringPainted(true);
 
         frame.add(bar);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
