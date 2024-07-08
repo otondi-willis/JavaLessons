173883f8035f5f610289b0b3b54fbcc6395be56d
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-08 09:33:07 +0300

    added frame

--- src/main/java/org/sajourney/JavaLessons/progressbars/ProgressBarDemo.java
+++ src/main/java/org/sajourney/JavaLessons/progressbars/ProgressBarDemo.java
@@ -1,4 +1,16 @@
 package org.sajourney.JavaLessons.progressbars;
 
+import javax.swing.*;
+
 public class ProgressBarDemo {
+
+    JFrame frame = new JFrame();
+    JProgressBar bar = new JProgressBar();
+    ProgressBarDemo(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,420);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+    }
 }
