9e9dfa617b6040b8e9683d205d178095c712598b
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-08 11:29:40 +0300

    added loaditem functionality to select a file

---
+++ src/main/java/org/sajourney/JavaLessons/selectfile/Main.java
@@ -0,0 +1,8 @@
+package org.sajourney.JavaLessons.selectfile;
+
+public class Main {
+    public static void main(String[] args){
+        MyFrame frame = new MyFrame();
+
+    }
+}
---
+++ src/main/java/org/sajourney/JavaLessons/selectfile/MyFrame.java
@@ -0,0 +1,62 @@
+package org.sajourney.JavaLessons.selectfile;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    JMenuItem loadItem ;
+    JMenuItem saveItem ;
+    JMenuItem exitItem ;
+
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(new FlowLayout());
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenu editMenu = new JMenu("Edit");
+        JMenu helpMenu = new JMenu("Help");
+
+
+        loadItem = new JMenuItem("Load");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+
+        loadItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        fileMenu.setMnemonic(KeyEvent.VK_F);// alt + f for file
+        loadItem.setMnemonic(KeyEvent.VK_L);// L shortcut
+
+
+        fileMenu.add(loadItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+
+
+
+
+
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(helpMenu);
+        this.setJMenuBar(menuBar);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==loadItem){
+            JFileChooser fileChooser = new JFileChooser();
+
+            fileChooser.showOpenDialog(null);//select file to open
+        }
+
+    }
+}
