933c2d880749ba40f3d3511db779c2e9053f231f
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-10 15:29:48 +0300

    Comparison between Binary and Linear search

--- src/main/java/org/sajourney/JavaLessons/dsa/BinarySearch.java
+++ src/main/java/org/sajourney/JavaLessons/dsa/BinarySearch.java
@@ -3,7 +3,7 @@ package org.sajourney.JavaLessons.dsa;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        int[] nums = {5,7,9,11,13};
+        int[] nums = {1,2,3,4,5,7,9,11,13};
         int target = 13;
         
         int result = binarySearch(nums, target);
@@ -17,11 +17,14 @@ public class BinarySearch {
     }
 
     private static int binarySearch(int[] nums, int target) {
+        int steps = 0;
         int left = 0;
         int right = nums.length - 1;
         while(left<=right){
+            steps++;
             int mid = (left + right)/2;
             if(nums[mid] == target){
+                System.out.println("Binary search steps" + steps);
                 return mid;
             } else if(nums[mid] < target){
                 left = mid + 1;
@@ -33,6 +36,7 @@ public class BinarySearch {
 
         }
 
+        System.out.println("Binary search steps" + steps);
         return -1;
     }
 }
--- src/main/java/org/sajourney/JavaLessons/dsa/LinearSearch.java
+++ src/main/java/org/sajourney/JavaLessons/dsa/LinearSearch.java
@@ -3,8 +3,8 @@ package org.sajourney.JavaLessons.dsa;
 public class LinearSearch {
 
     public static void main(String[] args) {
-        int[] nums = {5,7,9,11,13};
-        int target = 11;
+        int[] nums = {1,2,3,4,5,7,9,11,13};
+        int target = 13;
 
         int result = linearSearch(nums, target);
 
@@ -17,8 +17,11 @@ public class LinearSearch {
     }
 
     private static int linearSearch(int[] nums, int target) {
+        int steps = 0;
         for(int i=0;i<nums.length; i++){
+            steps++;
             if(nums[i]==target){
+                System.out.println("Steps taken by LinearSearch :" + steps);
                 return i;
 
             }
