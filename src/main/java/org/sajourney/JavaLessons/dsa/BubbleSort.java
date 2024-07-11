caedf54f163b15793400abdcc68d20aac9e35276
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-11 12:04:41 +0300

    bubble sort

---
+++ src/main/java/org/sajourney/JavaLessons/dsa/BubbleSort.java
@@ -0,0 +1,33 @@
+package org.sajourney.JavaLessons.dsa;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] nums = {6,8,2,5,4,1,9,7};
+        int size = nums.length;
+        int temp = 0;
+        System.out.println("Before sorting...");
+        for (int num : nums){
+            System.out.print(num + " ");
+        }
+        System.out.println(" ");
+
+        for(int i=0; i<size;i++){
+
+            for(int j=0; j<size-1; j++){
+                if(nums[j] > nums[j+1]){
+                    temp = nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
+            }
+
+        }
+
+        System.out.println("After sorting...");
+        for (int num : nums){
+            System.out.print(num + " ");
+        }
+
+
+    }
+}
