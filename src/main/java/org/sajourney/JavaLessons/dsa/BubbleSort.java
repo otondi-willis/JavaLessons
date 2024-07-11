f6c40b6f77ed4a92053400502c33e381cfa92d91
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-11 12:13:05 +0300

    Performance boost

--- src/main/java/org/sajourney/JavaLessons/dsa/BubbleSort.java
+++ src/main/java/org/sajourney/JavaLessons/dsa/BubbleSort.java
@@ -4,6 +4,7 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] nums = {6,8,2,5,4,1,9,7};
         int size = nums.length;
+        int steps=0;
         int temp = 0;
         System.out.println("Before sorting...");
         for (int num : nums){
@@ -13,7 +14,8 @@ public class BubbleSort {
 
         for(int i=0; i<size;i++){
 
-            for(int j=0; j<size-1; j++){
+            for(int j=0; j<size-i-1; j++){
+                steps++;
                 if(nums[j] > nums[j+1]){
                     temp = nums[j];
                     nums[j]=nums[j+1];
@@ -21,12 +23,16 @@ public class BubbleSort {
                 }
             }
 
+
+
         }
 
         System.out.println("After sorting...");
         for (int num : nums){
             System.out.print(num + " ");
         }
+        System.out.println();
+        System.out.println("Steps" + steps);
 
 
     }
