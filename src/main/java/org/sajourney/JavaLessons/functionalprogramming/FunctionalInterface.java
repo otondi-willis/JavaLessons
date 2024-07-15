2e0744149e2a6c9394cfc6717d376441222a654e
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-15 21:31:07 +0300

    Additional functional programming aspects

--- src/main/java/org/sajourney/JavaLessons/functionalprogramming/FunctionalInterface.java
+++ src/main/java/org/sajourney/JavaLessons/functionalprogramming/FunctionalInterface.java
@@ -4,6 +4,7 @@ import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class FunctionalInterface {
     public static void main(String[] args) {
@@ -14,5 +15,11 @@ public class FunctionalInterface {
         List countries = List.of("Australia","China","France","Italy");
         Consumer print = c -> System.out.println(c);
         countries.forEach(print);
+
+        Function<Integer, Integer> square = (n) -> n*n;
+        Function<Integer, Integer> add = (n) -> n+n;
+
+        int num = square.andThen(add).apply(5);
+        System.out.println(num);
     }
 }
