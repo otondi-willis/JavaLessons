ef9fa1a45bb6e669e790d2ca1ded8b2d7292c362
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-16 08:33:06 +0300

    completion of customized checked exceptions

--- src/main/java/org/sajourney/JavaLessons/exceptionhandling/ThrowableExceptions.java
+++ src/main/java/org/sajourney/JavaLessons/exceptionhandling/ThrowableExceptions.java
@@ -10,10 +10,15 @@ public class ThrowableExceptions {
     /*
      *
      * */
-    public static double calculatePay(double hours, double payRate){
+    public static double calculatePay(double hours, double payRate) throws NegativeInputException{
+        //throwing unchecked exceptions
         if(hours > 40){
         throw new IllegalArgumentException("Hours must be less than or equal to 40");
     }
+        //throwing checked exceptions
+        if (hours < 0 || payRate <0){
+            throw new NegativeInputException();
+        }
         return hours * payRate;
     }
     public static void main(String[] args) {
