3278eb069fad0b610f3d507e9cd29992d32d940a
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-16 08:29:27 +0300

    use of compile time exceptions

---
+++ src/main/java/org/sajourney/JavaLessons/exceptionhandling/NegativeInputException.java
@@ -0,0 +1,11 @@
+package org.sajourney.JavaLessons.exceptionhandling;
+
+public class NegativeInputException extends Exception{
+    public NegativeInputException(){
+        this("Input must be greater than or equal to 0");
+    }
+
+    public NegativeInputException(String message){
+        super(message);
+    }
+}
