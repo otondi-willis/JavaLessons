9afa153eeab8fc1266761f640a612091818b417f
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-17 14:40:24 +0300

    use of interfaces and the  implementations

---
+++ src/main/java/org/sajourney/JavaLessons/javacollections/Contract.java
@@ -0,0 +1,6 @@
+package org.sajourney.JavaLessons.javacollections;
+
+public interface Contract {
+    public void term1();
+    public void term2();
+}
---
+++ src/main/java/org/sajourney/JavaLessons/javacollections/Implementation.java
@@ -0,0 +1,13 @@
+package org.sajourney.JavaLessons.javacollections;
+
+public class Implementation implements Contract{
+    @Override
+    public void term1() {
+        System.out.println("implementation: term1");
+    }
+
+    @Override
+    public void term2() {
+        System.out.println("implementation: term2");
+    }
+}
