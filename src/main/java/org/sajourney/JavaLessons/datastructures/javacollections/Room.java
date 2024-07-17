1775f0def8b6f25880135b65a9eba210a100ce4f
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-17 13:53:53 +0300

    Created a Room class

---
+++ src/main/java/org/sajourney/JavaLessons/datastructures/javacollections/Room.java
@@ -0,0 +1,45 @@
+package org.sajourney.JavaLessons.datastructures.javacollections;
+
+public class Room {
+    private String name;
+    private String type;
+    private int capacity;
+    private double rate;
+
+    public Room(String name, String type, int capacity, double rate){
+        this.name=name;
+        this.type=type;
+        this.capacity=capacity;
+        this.rate=rate;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+}
