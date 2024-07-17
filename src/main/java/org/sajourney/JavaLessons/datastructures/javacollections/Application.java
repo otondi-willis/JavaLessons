4bfb803db58eaa3bbb6cc51ca37120d11ff7ed13
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-17 14:20:46 +0300

    use of collections for flexibility and extensibility

--- src/main/java/org/sajourney/JavaLessons/datastructures/javacollections/Application.java
+++ src/main/java/org/sajourney/JavaLessons/datastructures/javacollections/Application.java
@@ -1,14 +1,24 @@
 package org.sajourney.JavaLessons.datastructures.javacollections;
 
+import java.util.Collection;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         Room kiambu = new Room("Ruiru","Premier room",4,175.00);
+        Room siaya = new Room("Eastleigh","Suite",5,250.00);
+        Room kisumu = new Room("Eastleigh","Suite",5,250.00);
+
         Room nairobi = new Room("Eastleigh","Suite",5,250.00);
 
-        double total = getPotentialRevenue(kiambu,nairobi);
+        //Use of collections to manage rooms
+        Collection<Room> rooms = List.of(kiambu,nairobi,siaya,kisumu);
+        double total = getPotentialRevenue(rooms);
         System.out.println(total);
     }
-    private static double getPotentialRevenue(Room room1, Room room2){
-        return room1.getRate()+room2.getRate();
+    private static double getPotentialRevenue(Collection<Room> rooms){
+        return rooms.stream()
+                .mapToDouble(r->r.getRate())
+                .sum();
     }
 }
