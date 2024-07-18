a0d52d483a531b1cb8b20fca1828736ec1461355
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-18 03:30:05 +0300

    use of arraylist

--- src/main/java/org/sajourney/JavaLessons/javacollections/creatingcollections/Application.java
+++ src/main/java/org/sajourney/JavaLessons/javacollections/creatingcollections/Application.java
@@ -1,15 +1,23 @@
 package org.sajourney.JavaLessons.javacollections.creatingcollections;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 
 public class Application {
     public static void main(String[] args) {
-        Collection<String> c = new HashSet<>();
+        Collection<String> c = new ArrayList<>();
         c.add("snowboard");
         c.add("linux");
         c.add("books");
         c.add("snowboard");
+
+        /*
+        * another way of collection creation
+        * each collection implementation has a conversion constructor that
+        * accepts another collection as an argument*/
+
+        Collection<String> favoriteThings = new HashSet<>(c);
 
         System.out.println(c);
 
