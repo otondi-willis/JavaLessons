634575a701bc8d9c63e0011f7f6229d81aaabf85
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-18 05:25:12 +0300

    highlight of functional coding style using STREAMS

--- src/main/java/org/sajourney/JavaLessons/javacollections/streams/Application.java
+++ src/main/java/org/sajourney/JavaLessons/javacollections/streams/Application.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class Application {
     public static void main(String[] args) {
@@ -19,14 +21,23 @@ public class Application {
         piccadilly.setPetFriendly(true);
         victoria.setPetFriendly(true);
 
-        Iterator<Room> iterator = rooms.iterator();
-        while (iterator.hasNext()){
-            Room room = iterator.next();
-            if(room.isPetFriendly())
-            {
-                iterator.remove();
-            }
-        }
+        //stream as a pipeline that elements flow through
+
+        rooms.stream() //invokation to start the pipeline
+                .filter(new Predicate<Room>() {
+                    @Override
+                    public boolean test(Room room) {
+                        System.out.format("Testing %s with result %b%n", room.getName(),room.isPetFriendly());
+                        return room.isPetFriendly();
+                    }
+                }).forEach(new Consumer<Room>() {
+                    @Override
+                    public void accept(Room room) {
+                        System.out.println(room.getName());
+                    }
+                });//terminal operation(return a result or modify elements that are streamed into it)
+
+
 
 
 
