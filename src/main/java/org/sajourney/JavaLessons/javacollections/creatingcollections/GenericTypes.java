c1a504eb1b030e3098a2c2078a41bd28b9ce00a1
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-18 03:48:22 +0300

    ClassCast exception addressed

--- src/main/java/org/sajourney/JavaLessons/javacollections/creatingcollections/GenericTypes.java
+++ src/main/java/org/sajourney/JavaLessons/javacollections/creatingcollections/GenericTypes.java
@@ -7,12 +7,13 @@ import java.util.Collection;
 
 public class GenericTypes {
     public static void main(String[] args) {
-        Collection c = new ArrayList();
-        c.add("Cambridge");
-        c.add(1L);
+        Collection<Room> c = new ArrayList<>();//fail fast and fail early than without using Generic Typing
+
+//        c.add("Cambridge");
+//        c.add(1L);
         c.add(new Room("Cambridge","Suite",6,250.0));
         c.stream()
-                .forEach(e-> System.out.println(((Room)e).getClass()));
+                .forEach(e-> System.out.println(e.getName()));
 
         Collection c2 = new ArrayList();
         c2.add(new Room("Cambridge","Suite",6,250.0));
