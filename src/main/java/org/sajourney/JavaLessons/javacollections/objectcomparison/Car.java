0887e2aa1daf0a31df04e200f50d9915a2827842
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-18 04:17:00 +0300

    use of equals method and hashcode

--- src/main/java/org/sajourney/JavaLessons/javacollections/objectcomparison/Car.java
+++ src/main/java/org/sajourney/JavaLessons/javacollections/objectcomparison/Car.java
@@ -1,9 +1,25 @@
 package org.sajourney.JavaLessons.javacollections.objectcomparison;
 
+import java.util.Objects;
+
 public class Car {
+
     private String make;
     private String model;
     private int mileage;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car car)) return false;
+        return Objects.equals(getMake(), car.getMake()) && Objects.equals(getModel(), car.getModel());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getMake(), getModel());
+    }
+
     public Car(String make, String model, int mileage){
         this.make=make;
         this.model=model;
