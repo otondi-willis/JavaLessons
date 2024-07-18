cc5344cb7bd3daed19594241678b13a291ff4912
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-18 04:31:47 +0300

    created application class

---
+++ src/main/java/org/sajourney/JavaLessons/javacollections/iterators/Application.java
@@ -0,0 +1,19 @@
+package org.sajourney.JavaLessons.javacollections.iterators;
+
+import org.sajourney.JavaLessons.datastructures.javacollections.Room;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class Application {
+    public static void main(String[] args) {
+        Room cambridge = new Room("Cambridge","Premier",6,175.00);
+        Room piccadilly = new Room("Picaddilly","Guest Room",3,125.00);
+        Room westminister = new Room("Westminister","Premier Room",4,200.00);
+
+        Collection<Room> rooms = new ArrayList<>(Arrays.asList(piccadilly,cambridge,westminister));
+
+
+    }
+}
