99eb4e2d43dd2ce2f0b2288bf7df9c24e6fbce62
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-18 05:13:59 +0300

    highlight of imperative coding style

---
+++ src/main/java/org/sajourney/JavaLessons/javacollections/streams/Application.java
@@ -0,0 +1,34 @@
+package org.sajourney.JavaLessons.javacollections.streams;
+
+import org.sajourney.JavaLessons.datastructures.javacollections.Room;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Application {
+    public static void main(String[] args) {
+        //Highlight of imperative coding style
+        Room cambridge = new Room("Cambridge","Premier",6,175.00);
+        Room piccadilly = new Room("Picaddilly","Guest Room",3,125.00);
+        Room westminister = new Room("Westminister","Premier Room",4,200.00);
+        Room victoria = new Room("Victoria","Suite",5,225.00);
+
+        Collection<Room> rooms = new ArrayList<>(Arrays.asList(piccadilly,cambridge,westminister,victoria));
+        piccadilly.setPetFriendly(true);
+        victoria.setPetFriendly(true);
+
+        Iterator<Room> iterator = rooms.iterator();
+        while (iterator.hasNext()){
+            Room room = iterator.next();
+            if(room.isPetFriendly())
+            {
+                iterator.remove();
+            }
+        }
+
+
+
+    }
+}
