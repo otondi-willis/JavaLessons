42d5bff69e20aef8ee521b2864ad1c037e53c43f
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-18 03:34:26 +0300

    good practice tips added

--- src/main/java/org/sajourney/JavaLessons/javacollections/creatingcollections/Application.java
+++ src/main/java/org/sajourney/JavaLessons/javacollections/creatingcollections/Application.java
@@ -6,6 +6,10 @@ import java.util.HashSet;
 
 public class Application {
     public static void main(String[] args) {
+        /*
+         * use the most abstract type possible for the type of your object
+         * for easier implementation switch without challenges
+         * */
         Collection<String> c = new ArrayList<>();
         c.add("snowboard");
         c.add("linux");
