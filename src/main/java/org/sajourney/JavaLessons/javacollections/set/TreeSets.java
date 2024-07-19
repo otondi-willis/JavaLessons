89a5a5dc69c968f683fa36af379632703717c267
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-19 05:26:55 +0300

    TreeSet implementation

---
+++ src/main/java/org/sajourney/JavaLessons/javacollections/set/TreeSets.java
@@ -0,0 +1,18 @@
+package org.sajourney.JavaLessons.javacollections.set;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class TreeSets {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(500,1500,2500,1000,3000,2000);
+
+        NavigableSet<Integer> numberTree = new TreeSet<>(numbers);
+
+        numberTree.stream()
+                .forEach(System.out::println);
+
+    }
+}
