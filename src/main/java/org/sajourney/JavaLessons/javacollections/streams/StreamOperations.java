638072bf04e67a248a15526ec9e319575c434670
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-19 03:47:29 +0300

    passing in external object and modifying it within a stream

---
+++ src/main/java/org/sajourney/JavaLessons/javacollections/streams/StreamOperations.java
@@ -0,0 +1,41 @@
+package org.sajourney.JavaLessons.javacollections.streams;
+
+import org.sajourney.JavaLessons.datastructures.javacollections.Room;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class StreamOperations {
+    public static void main(String[] args) {
+        //Highlight of imperative coding style
+        Room cambridge = new Room("Cambridge","Premier",6,175.00);
+        Room piccadilly = new Room("Picaddilly","Guest Room",3,125.00);
+        Room westminister = new Room("Westminister","Premier Room",4,200.00);
+        Room victoria = new Room("Victoria","Suite",5,225.00);
+
+        Collection<Room> rooms = new ArrayList<>(Arrays.asList(piccadilly,cambridge,westminister,victoria));
+        piccadilly.setPetFriendly(true);
+        victoria.setPetFriendly(true);
+
+        Collection<Room> petFriendlyRooms = new ArrayList<>();
+
+        //stream as a pipeline that elements flow through
+
+        rooms.stream() //invokation to start the pipeline
+                .filter(Room::isPetFriendly)
+                .forEach(room->petFriendlyRooms.add(room));//terminal operation(return a result or modify elements that are streamed into it)
+
+        //passing in external object and modifying it within a stream
+        //should never use both
+        petFriendlyRooms.stream()
+                .forEach(r-> System.out.println(r.getName()));
+
+
+
+
+    }
+}
