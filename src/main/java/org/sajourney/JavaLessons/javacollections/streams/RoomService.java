97e1b0bfcad9d82b4aabe1eec6b69dee6dcd3ca9
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-19 04:32:25 +0300

    RoomService exercise part 2

--- src/main/java/org/sajourney/JavaLessons/javacollections/streams/RoomService.java
+++ src/main/java/org/sajourney/JavaLessons/javacollections/streams/RoomService.java
@@ -3,14 +3,28 @@ package org.sajourney.JavaLessons.javacollections.streams;
 import org.sajourney.JavaLessons.datastructures.javacollections.Room;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 
 public class RoomService {
+    public Collection<Room> inventory;
     public RoomService(){
         this.inventory = new LinkedHashSet<>();
     }
+    public void applyDiscount(final double discount){
+        this.inventory.forEach(r->r.setRate(r.getRate()*(1-discount)));
+    }
     public Collection<Room> getRoomsByCapacity(final int requiredCapacity){
-        return null;
+        Collection<Room> matches = new HashSet<>();
+        for (Room room:inventory){
+            if(room.getCapacity() >= requiredCapacity){
+                matches.add(room);
+            }
+
+        }
+
+
+        return matches;
     }
     public Collection<Room> getRoomByRateAndType(final double rate, final String type){
         return null;
