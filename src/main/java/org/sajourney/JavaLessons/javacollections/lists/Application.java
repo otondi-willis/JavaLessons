24587eb6c0927ac88d31cf43a0e2d6eac4116c75
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-20 09:20:12 +0300

    added guests to the list

---
+++ src/main/java/org/sajourney/JavaLessons/javacollections/lists/Application.java
@@ -0,0 +1,15 @@
+package org.sajourney.JavaLessons.javacollections.lists;
+
+public class Application {
+    public static void main(String[] args) {
+        Guest john = new Guest("john", "doe",false);
+        Guest maria = new Guest("maria", "carrey", false);
+        Guest sonia = new Guest("sonia","amolo",true);
+        Guest siri = new Guest("siri","doe",true);
+
+
+    }
+    public static void print(){
+
+    }
+}
