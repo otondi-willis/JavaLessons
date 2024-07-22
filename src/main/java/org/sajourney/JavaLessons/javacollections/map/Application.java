ee2ace5ca5ce18c10fe83e2368904013d8cb034b
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-22 22:06:57 +0300

    using map methods

---
+++ src/main/java/org/sajourney/JavaLessons/javacollections/map/Application.java
@@ -0,0 +1,26 @@
+package org.sajourney.JavaLessons.javacollections.map;
+
+import org.sajourney.JavaLessons.datastructures.javacollections.Room;
+import org.sajourney.JavaLessons.javacollections.lists.Guest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Application {
+    public static void main(String[] args) {
+        Room piccadilly = new Room("piccadilly", "Guest room",3,125);
+        Room oxford = new Room("oxford","suite",5,224);
+
+        Guest john = new Guest("John","Doe",false);
+        Guest maria = new Guest("maria", "doe",true);
+
+        Map<Room,Guest> assignments = new HashMap<>();
+        assignments.put(oxford,maria);
+        assignments.put(piccadilly,john);
+
+        System.out.println("oxford: " + assignments.get(new Room("oxford","suite",5,224)));
+        System.out.println("Piccadilly: " + assignments.get(piccadilly));
+
+
+    }
+}
