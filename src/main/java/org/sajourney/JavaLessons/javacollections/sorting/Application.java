2dc90d0b43d0746f2ef7feff22b9eb614c2d41a1
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-22 22:51:22 +0300

    rooms placed in their insertion order

---
+++ src/main/java/org/sajourney/JavaLessons/javacollections/sorting/Application.java
@@ -0,0 +1,24 @@
+package org.sajourney.JavaLessons.javacollections.sorting;
+
+import org.sajourney.JavaLessons.datastructures.javacollections.Room;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Application {
+
+        public static void main(String[] args) {
+            Room kiambu = new Room("Ruiru","Premier room",4,175.00);
+            Room siaya = new Room("Eastleigh","Suite",5,250.00);
+            Room kisumu = new Room("Eastleigh","Suite",5,250.00);
+
+           List<Room> rooms = new ArrayList<>(List.of(kiambu,siaya,kisumu));
+           rooms.stream()
+                   .forEach(r-> System.out.format("%-15s %-15s %-10f %n", r.getName(),r.getType(),r.getRate()));
+
+
+        }
+
+
+}
