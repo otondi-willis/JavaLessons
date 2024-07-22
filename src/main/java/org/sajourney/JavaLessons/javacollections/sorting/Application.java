bf8109b4d8457f4683fdb6393e26ae34fd70664f
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-22 22:52:47 +0300

    sorting rooms with Collections.sort

--- src/main/java/org/sajourney/JavaLessons/javacollections/sorting/Application.java
+++ src/main/java/org/sajourney/JavaLessons/javacollections/sorting/Application.java
@@ -4,19 +4,21 @@ import org.sajourney.JavaLessons.datastructures.javacollections.Room;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Application {
 
         public static void main(String[] args) {
             Room kiambu = new Room("Ruiru","Premier room",4,175.00);
-            Room siaya = new Room("Eastleigh","Suite",5,250.00);
+            Room siaya = new Room("Eastleigh","Suite1",5,250.00);
             Room kisumu = new Room("Eastleigh","Suite",5,250.00);
 
            List<Room> rooms = new ArrayList<>(List.of(kiambu,siaya,kisumu));
            rooms.stream()
                    .forEach(r-> System.out.format("%-15s %-15s %-10f %n", r.getName(),r.getType(),r.getRate()));
 
+            Collections.sort(rooms);
 
         }
 
