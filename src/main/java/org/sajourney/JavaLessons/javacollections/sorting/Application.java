9e8638a3e7b4a4bce30792b2f13ca6d99360fc72
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-28 14:33:49 +0300

    use of rate comparator to sort

--- src/main/java/org/sajourney/JavaLessons/javacollections/sorting/Application.java
+++ src/main/java/org/sajourney/JavaLessons/javacollections/sorting/Application.java
@@ -8,11 +8,11 @@ public class Application {
 
         public static void main(String[] args) {
             Room kiambu = new Room("Ruiru","Premier room",4,175.00);
-            Room siaya = new Room("Eastleigh","Suite1",5,250.00);
+            Room siaya = new Room("Eastleigh","Suite1",5,1250.00);
             Room kisumu = new Room("Eastleigh","Suite1",6,251.00);
 
            List<Room> rooms = new ArrayList<>(List.of(kiambu,siaya,kisumu));
-            rooms.sort(Comparator.naturalOrder());
+            rooms.sort(Room.RATE_COMPARATOR);
            rooms.stream()
                    .forEach(r-> System.out.format("%-15s %-15s %-10f %n", r.getName(),r.getType(),r.getRate()));
 
