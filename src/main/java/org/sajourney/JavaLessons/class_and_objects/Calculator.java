562820ff9944121df648d2f8050e81045817dd3d
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-15 03:42:07 +0300

    method overloading in action

--- src/main/java/org/sajourney/JavaLessons/class_and_objects/Calculator.java
+++ src/main/java/org/sajourney/JavaLessons/class_and_objects/Calculator.java
@@ -5,10 +5,14 @@ public class Calculator {
     public int addition (int num1, int num2){
         return num1 + num2;
     }
+    public int addition (int num1, int num2, int num3){
+        return num1 + num2 + num3;
+    }
 }
  class Main{
     public static void main(String[] args){
         Calculator calc = new Calculator();
-        System.out.println(calc.addition(15,15));
+        System.out.println("Addition of two integers " + calc.addition(15,15));
+        System.out.println("Addition of three integers " + calc.addition(15,15,50));
     }
 }
