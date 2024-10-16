95011a353dc094602ddf693efba3a2260e68323c
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-16 04:27:18 +0300

    array examples. reinitializing element's value

--- src/main/java/org/sajourney/JavaLessons/arrays/Demo.java
+++ src/main/java/org/sajourney/JavaLessons/arrays/Demo.java
@@ -4,7 +4,8 @@ public class Demo {
     public static void main(String[] args){
         int nums[]={3,7,2,5};
         System.out.println(nums[0]);//printing first value
-        nums[2]=9;
+        nums[2]=9;//reinitializing the value of the third element
+        System.out.println(nums[2]);
 
 
     }
