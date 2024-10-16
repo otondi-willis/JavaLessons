752aca82ab8f81f2298aeb90682e15358664eaa1
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-16 04:25:30 +0300

    array examples. printing the first value

---
+++ src/main/java/org/sajourney/JavaLessons/arrays/Demo.java
@@ -0,0 +1,11 @@
+package org.sajourney.JavaLessons.arrays;
+
+public class Demo {
+    public static void main(String[] args){
+        int nums[]={3,7,2,5};
+        System.out.println(nums[0]);//printing first value
+        nums[2]=9;
+
+
+    }
+}
