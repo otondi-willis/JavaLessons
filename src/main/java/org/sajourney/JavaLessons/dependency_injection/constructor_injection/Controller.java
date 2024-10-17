1dce4c80722000410f57209e4427752edfd6a7d8
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-17 05:18:15 +0300

    constructor dependency injection

--- src/main/java/org/sajourney/JavaLessons/dependency_injection/constructor_injection/Controller.java
+++ src/main/java/org/sajourney/JavaLessons/dependency_injection/constructor_injection/Controller.java
@@ -2,6 +2,11 @@ package org.sajourney.JavaLessons.dependency_injection.constructor_injection;
 
 public class Controller {
     private static Service service;
+
+    //Constructor injection
+    public Controller( Service service){
+        this.service = service;
+    }
     public static void main(String[] args){
      service.handleSomething();
     }
