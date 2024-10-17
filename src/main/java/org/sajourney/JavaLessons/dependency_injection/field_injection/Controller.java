38dcb0595b16fb6c9513b4e6d6cfa19ffa44fbb7
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-17 05:23:27 +0300

    field dependency injection

---
+++ src/main/java/org/sajourney/JavaLessons/dependency_injection/field_injection/Controller.java
@@ -0,0 +1,13 @@
+package org.sajourney.JavaLessons.dependency_injection.field_injection;
+
+public class Controller {
+
+    //field injection
+    @Autowired
+    private static Service service;
+
+    public static void main(String[] args){
+     service.handleSomething();
+    }
+
+}
