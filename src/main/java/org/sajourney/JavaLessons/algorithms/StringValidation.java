3036d0058dde9fd69df6c45bf95ee0484eb19da7
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-19 10:33:02 +0300

    string validation to check if it has any digit

--- src/main/java/org/sajourney/JavaLessons/algorithms/StringValidation.java
+++ src/main/java/org/sajourney/JavaLessons/algorithms/StringValidation.java
@@ -8,9 +8,14 @@ public class StringValidation {
     public static boolean isLowerCase(String s){
         return s.chars().anyMatch(Character::isLowerCase);
     }
+    public static boolean isDigits(String s){
+        return s.chars().anyMatch(Character::isDigit);
+    }
+    public static boolean
     public static void main(String[] args){
-        System.out.println(isUpperCase("omera"));
+        System.out.println(isUpperCase("omEra"));
         System.out.println(isLowerCase("omera"));
+        System.out.println(isDigits("omera1"));
     }
 
 }
