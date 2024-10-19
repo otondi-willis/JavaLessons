32fa09019d4f7d2b40bae9d823065fadf8e6adac
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-19 11:28:44 +0300

    feat: content parsing using for loop

--- src/main/java/org/sajourney/JavaLessons/algorithms/SearchAlgo.java
+++ src/main/java/org/sajourney/JavaLessons/algorithms/SearchAlgo.java
@@ -3,11 +3,17 @@ package org.sajourney.JavaLessons.algorithms;
 import org.w3c.dom.ls.LSOutput;
 
 public class SearchAlgo {
-    public String parseContents(String s){
+    public void parseContents(String s){
+        System.out.println("Option 1");
         for(char c:s.toCharArray()){
             System.out.println(c);
         }
         System.out.println();
+        System.out.println("Option 2");
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            System.out.println(c);
+        }
     }
 
     public static void main(String[] args){
