4219b846fe0e75d2c889e316a70cf0bd12156fd9
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-19 10:28:57 +0300

    string validation to check if it has any lowercase letter

--- src/main/java/org/sajourney/JavaLessons/algorithms/StringValidation.java
+++ src/main/java/org/sajourney/JavaLessons/algorithms/StringValidation.java
@@ -5,8 +5,12 @@ public class StringValidation {
     public static boolean isUpperCase(String s){
         return s.chars().anyMatch(Character::isUpperCase);
     }
+    public static boolean isLowerCase(String s){
+        return s.chars().anyMatch(Character::isLowerCase);
+    }
     public static void main(String[] args){
         System.out.println(isUpperCase("omera"));
+        System.out.println(isLowerCase("omera"));
     }
 
 }
