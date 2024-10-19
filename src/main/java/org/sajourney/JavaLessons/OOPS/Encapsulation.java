f0737662c113571c0cbbb957bab8b47ab14d3a4b
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-19 09:04:45 +0300

    retrieving instance variable values using Getters

--- src/main/java/org/sajourney/JavaLessons/OOPS/Encapsulation.java
+++ src/main/java/org/sajourney/JavaLessons/OOPS/Encapsulation.java
@@ -3,6 +3,14 @@ package org.sajourney.JavaLessons.OOPS;
 class Example{
     private String name;
     private int age;
+
+
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
 }
 public class Encapsulation {
     Example example=new Example();
