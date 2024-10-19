bde50b8b32bf9e9ed5415c2bdd00af1f1b6b09ac
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-19 11:25:24 +0300

    feat: content parsing using enhanced for loop

--- src/main/java/org/sajourney/JavaLessons/algorithms/SearchAlgo.java
+++ src/main/java/org/sajourney/JavaLessons/algorithms/SearchAlgo.java
@@ -3,7 +3,12 @@ package org.sajourney.JavaLessons.algorithms;
 import org.w3c.dom.ls.LSOutput;
 
 public class SearchAlgo {
-
+    public String parseContents(String s){
+        for(char c:s.toCharArray()){
+            System.out.println(c);
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args){
         String s = "HelloWorld";
