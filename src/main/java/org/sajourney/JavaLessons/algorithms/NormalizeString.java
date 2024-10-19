9986919fab4507849a28525088542683be5c753e
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-19 11:06:52 +0300

    feat: simplified String normalization algorithm

--- src/main/java/org/sajourney/JavaLessons/algorithms/NormalizeString.java
+++ src/main/java/org/sajourney/JavaLessons/algorithms/NormalizeString.java
@@ -2,11 +2,9 @@ package org.sajourney.JavaLessons.algorithms;
 
 public class NormalizeString {
     public static String normalizedString(String s){
-        String lowerCase = s.toLowerCase();
-        String trimmed = lowerCase.trim();
-        String normalized = trimmed.replace(",","");
+        return s.toLowerCase().trim().replace(",","");
 
-        return normalized;
+
     }
     public static void main(String[] args){
         System.out.println(normalizedString("my name is WILLIS, OTONDI"));
