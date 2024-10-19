7630b385d7ad448e514ac505c3ddcacde1c482ae
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-19 09:02:12 +0300

    use of encapsulation

---
+++ src/main/java/org/sajourney/JavaLessons/OOPS/Encapsulation.java
@@ -0,0 +1,10 @@
+package org.sajourney.JavaLessons.OOPS;
+
+class Example{
+    private String name;
+    private int age;
+}
+public class Encapsulation {
+    Example example=new Example();
+    //example.name; name has private access
+}
