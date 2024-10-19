e5046cf6f654ebaa10451e41f92f5a366d22610d
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-19 11:23:37 +0300

    feat: string search algorithm using contains method

---
+++ src/main/java/org/sajourney/JavaLessons/algorithms/SearchAlgo.java
@@ -0,0 +1,12 @@
+package org.sajourney.JavaLessons.algorithms;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class SearchAlgo {
+
+
+    public static void main(String[] args){
+        String s = "HelloWorld";
+        System.out.println(s.contains("lll"));
+    }
+}
