b0edbf3b5ab5faf46b2ce2b6d7abaab17e0211ec
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-21 06:42:38 +0300

    feat: null check

---
+++ src/main/java/org/sajourney/JavaLessons/algorithms/AlgorithmDrivenString.java
@@ -0,0 +1,12 @@
+package org.sajourney.JavaLessons.algorithms;
+
+public class AlgorithmDrivenString {
+    public static String reverse(String s){
+        if(s == null || s.isEmpty()){
+            return s;
+        }
+    }
+    public static void main (String[] args){
+
+    }
+}
--- src/main/java/org/sajourney/JavaLessons/algorithms/SearchAlgo.java
+++ src/main/java/org/sajourney/JavaLessons/algorithms/SearchAlgo.java
@@ -3,21 +3,23 @@ package org.sajourney.JavaLessons.algorithms;
 import org.w3c.dom.ls.LSOutput;
 
 public class SearchAlgo {
-    public void parseContents(String s){
+    public static void parseContents(String s){
         System.out.println("Option 1");
         for(char c:s.toCharArray()){
-            System.out.println(c);
+            System.out.print(c);
         }
         System.out.println();
         System.out.println("Option 2");
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
-            System.out.println(c);
+            System.out.print(c);
         }
     }
 
     public static void main(String[] args){
         String s = "HelloWorld";
         System.out.println(s.contains("lll"));
+        parseContents("Omondi Steven");
+
     }
 }
