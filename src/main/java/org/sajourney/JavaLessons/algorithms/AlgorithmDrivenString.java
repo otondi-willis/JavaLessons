88611d54e60cebd33ae653df34fba481158178ea
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-21 06:49:14 +0300

    feat: algorithm to return reverse of Strings

--- src/main/java/org/sajourney/JavaLessons/algorithms/AlgorithmDrivenString.java
+++ src/main/java/org/sajourney/JavaLessons/algorithms/AlgorithmDrivenString.java
@@ -5,8 +5,16 @@ public class AlgorithmDrivenString {
         if(s == null || s.isEmpty()){
             return s;
         }
+        StringBuilder reversed = new StringBuilder();
+        for (int i=s.length()-1;i>=0;i--){
+            reversed.append(s.charAt(i));
+        }
+        return reversed.toString();
     }
     public static void main (String[] args){
+        System.out.println(reverse(null));
+        System.out.println(reverse("Omera"));
+        System.out.println(reverse("Austin"));
 
     }
 }
