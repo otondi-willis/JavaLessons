afda20a7f90a629958c3cfe80262058a9d6f6c85
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-22 04:35:16 +0300

    feat: performing a linear search array algorithm that returns the item or the null object.

--- src/main/java/org/sajourney/JavaLessons/algorithms/LinearSearchArrays.java
+++ src/main/java/org/sajourney/JavaLessons/algorithms/LinearSearchArrays.java
@@ -1,13 +1,13 @@
 package org.sajourney.JavaLessons.algorithms;
 
 public class LinearSearchArrays {
-    public static int linearSearch(int[] arr, int item){
+    public static Integer linearSearch(int[] arr, int item){
         for (int current:arr){
             if (current == item){
                 return item;
             }
         }
-        return -1;
+        return null;
     }
     public static void main(String[] args){
         int[] arr={5,9,3,2,2,6,8};
