2d6b7d33522b3d39bc0bba41979c2600998d7d17
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-22 04:34:05 +0300

    feat: performing a linear search array algorithm that returns the item or the sentinent value.

--- src/main/java/org/sajourney/JavaLessons/algorithms/LinearSearchArrays.java
+++ src/main/java/org/sajourney/JavaLessons/algorithms/LinearSearchArrays.java
@@ -1,13 +1,13 @@
 package org.sajourney.JavaLessons.algorithms;
 
 public class LinearSearchArrays {
-    public static boolean linearSearch(int[] arr, int item){
+    public static int linearSearch(int[] arr, int item){
         for (int current:arr){
             if (current == item){
-                return true;
+                return item;
             }
         }
-        return false;
+        return -1;
     }
     public static void main(String[] args){
         int[] arr={5,9,3,2,2,6,8};
