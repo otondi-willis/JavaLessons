25f10b09c61577a5771bc73cae8123f1776490d6
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-22 05:14:28 +0300

    feat: performing a binary search on sorted array

---
+++ src/main/java/org/sajourney/JavaLessons/algorithms/BinarySearch.java
@@ -0,0 +1,29 @@
+package org.sajourney.JavaLessons.algorithms;
+
+public class BinarySearch {
+    public static boolean binarySearch(int[] arr, int item){
+        int min = 0;
+        int max = arr.length-1;
+
+        while(min <= max){
+            int mid = (max+min)/2;
+            if(item==arr[mid]){
+                return true;
+
+            }else if (item<arr[mid]){
+                max = mid - 1;
+            } else {
+                min = mid +1;
+            }
+
+
+        }
+        return false;
+    }
+
+    public static void main (String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8};
+        System.out.println(binarySearch(arr,9));
+
+    }
+}
