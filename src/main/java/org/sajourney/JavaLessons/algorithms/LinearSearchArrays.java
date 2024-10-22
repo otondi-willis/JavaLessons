41651ac3ef86787dba01563fbc1336cf9ee6ee50
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-22 04:39:28 +0300

    feat: performing a linear search array algorithm using stream

--- src/main/java/org/sajourney/JavaLessons/algorithms/LinearSearchArrays.java
+++ src/main/java/org/sajourney/JavaLessons/algorithms/LinearSearchArrays.java
@@ -1,5 +1,8 @@
 package org.sajourney.JavaLessons.algorithms;
 
+import java.util.Arrays;
+import java.util.OptionalInt;
+
 public class LinearSearchArrays {
     public static Integer linearSearch(int[] arr, int item){
         for (int current:arr){
@@ -9,10 +12,14 @@ public class LinearSearchArrays {
         }
         return null;
     }
+    public static OptionalInt linearSearch2(int[] arr, int item){
+        return Arrays.stream(arr).filter(x->x==item).findFirst();
+    }
     public static void main(String[] args){
         int[] arr={5,9,3,2,2,6,8};
 
         System.out.println(linearSearch(arr,1));
+        System.out.println(linearSearch2(arr,3));
 
     }
 }
