7cc3813620789c8295df5db9ce8c6864628a4963
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-12 05:44:27 +0300

    feat: time complexity demo2

--- src/main/java/org/sajourney/JavaLessons/dsa/TimeComplexityDemo.java
+++ src/main/java/org/sajourney/JavaLessons/dsa/TimeComplexityDemo.java
@@ -12,8 +12,15 @@ public class TimeComplexityDemo {
 
     }
 
-    private int findSum(int n) {
-        return n*(n+1)/2;
+//    public int findSum(int n) {
+//        return n*(n+1)/2;
+//    }
+    public int findSum(int n){
+        int sum = 0;
+        for(int i = 1; i<=n;i++){
+            sum +=i;
+        }
+        return sum;
     }
 
 }
