9e68d221613171a8331d44dd6e225f3671b1aa02
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-14 20:59:00 +0300

    feat: reversing array elements

--- src/main/java/org/sajourney/JavaLessons/dsa/arrays/ArrayUtil.java
+++ src/main/java/org/sajourney/JavaLessons/dsa/arrays/ArrayUtil.java
@@ -1,6 +1,26 @@
 package org.sajourney.JavaLessons.dsa.arrays;
 
 public class ArrayUtil {
-
+  public static void printArray(int[] arr){
+      int n = arr.length;
+      for(int i=0;i<n;i++){
+          System.out.print(arr[i] + " ");
+      }
+      System.out.println();
+  }
+    public static void reverseArray(int[] arr){
+        int n = arr.length-1;
+        for(int i=n;i>=0;i--){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+  public static void main(String[] args){
+      int[] array = new int[] {2,4,3,5,7};
+      System.out.println("Print array elements");
+      printArray(array);
+      System.out.println("Reverse array elements");
+      reverseArray(array);
+  }
 
 }
