c5ef786c645cae5ce2628b67390d5a965f8ad939
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-14 15:29:02 +0300

    feat: array with even integers removed

--- src/main/java/org/sajourney/JavaLessons/dsa/arrays/ArrayUtil.java
+++ src/main/java/org/sajourney/JavaLessons/dsa/arrays/ArrayUtil.java
@@ -8,6 +8,16 @@ public class ArrayUtil {
         }
         System.out.println();
     }
+    public void printEvenArray(int[] arr){
+        int n = arr.length;
+        System.out.println("Even array elements: ");
+        for(int i=0; i<n;i++){
+            if(arr[i]%2!=0)
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+    }
     public void arrayDemo(){
         int[] myArray = new int[5];
         printArray(myArray);
@@ -17,6 +27,7 @@ public class ArrayUtil {
 
         ArrayUtil arrUtil = new ArrayUtil();
         arrUtil.printArray(new int[] {3,4,5,6,4});
+        arrUtil.printEvenArray(new int[] {3,4,5,6,4});
         arrUtil.arrayDemo();
     }
 
