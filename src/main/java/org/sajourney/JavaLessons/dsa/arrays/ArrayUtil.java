883fc8f460bfafad01839fe334b2196d28cbaccc
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-14 14:58:01 +0300

    feat: added printArray method's functionality

--- src/main/java/org/sajourney/JavaLessons/dsa/arrays/ArrayUtil.java
+++ src/main/java/org/sajourney/JavaLessons/dsa/arrays/ArrayUtil.java
@@ -3,10 +3,19 @@ package org.sajourney.JavaLessons.dsa.arrays;
 public class ArrayUtil {
     public void printArray(int[] arr){
         int n = arr.length;
-        for (int i=0; i<n;i++){
+        for(int i=0; i<n;i++){
             System.out.print(arr[i] + " ");
         }
         System.out.println();
+    }
+    public void arrayDemo(){
+        int[] myArray = new int[5];
+        printArray(myArray);
+    }
+
+    public static void main(String[] args){
+        ArrayUtil arrUtil = new ArrayUtil();
+        arrUtil.arrayDemo();
     }
 
 }
