760c73898a38b07d8db09801ee40ae0f668bee7f
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-14 14:52:47 +0300

    feat: added printArray method

---
+++ src/main/java/org/sajourney/JavaLessons/dsa/arrays/ArrayUtil.java
@@ -0,0 +1,12 @@
+package org.sajourney.JavaLessons.dsa.arrays;
+
+public class ArrayUtil {
+    public void printArray(int[] arr){
+        int n = arr.length;
+        for (int i=0; i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
