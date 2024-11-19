bab80450a17db23ee3c3440b1c2bb9c8292b4fed
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-19 08:27:38 +0300

    feat: algorithm to find second largest array element

--- src/main/java/org/sajourney/JavaLessons/dsa/arrays/ArrayUtil.java
+++ src/main/java/org/sajourney/JavaLessons/dsa/arrays/ArrayUtil.java
@@ -103,8 +103,22 @@ public class ArrayUtil {
         }
         return max;
     }
+    public static int findSecondMax(int[] arr){
+      int max = Integer.MIN_VALUE;
+      int secondMax = Integer.MIN_VALUE;
+
+      for(int i=0; i<arr.length;i++){
+          if (arr[i]>max){
+              secondMax = max;
+              max=arr[i];
+          } else if(arr[i]>secondMax && arr[i]!=max){
+              secondMax=arr[i];
+          }
+      }
+      return secondMax;
+    }
   public static void main(String[] args){
-      int[] array = new int[] {4,6,1,9,3,2,4,3,5,7,0};
+      int[] array = new int[] {4,6,1,9,3,8,2,4,3,5,7,0};
       System.out.println("Print array elements");
       printArray(array);
 
@@ -137,6 +151,9 @@ public class ArrayUtil {
       System.out.println("Minimum value in the array: " + min);
       int max = findMax(array);
       System.out.println("Maximum value in the array: " + max);
+
+      int max1 = findSecondMax(array);
+      System.out.println("Maximum value in the array: " + max1);
 
 
 
