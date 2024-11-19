57728582045dc6ebd4d969b0494c69974f0b250c
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-19 08:46:27 +0300

    feat: added edge case check to findSecondMin method

--- src/main/java/org/sajourney/JavaLessons/dsa/arrays/ArrayFunctions.java
+++ src/main/java/org/sajourney/JavaLessons/dsa/arrays/ArrayFunctions.java
@@ -1,10 +1,13 @@
 package org.sajourney.JavaLessons.dsa.arrays;
 
 public class ArrayFunctions {
-    public static int[] findSecondMin(int[] arr){
+    public static int findSecondMin(int[] arr){
+        //edge case check
         if(arr==null || arr.length==0){
-
+            throw new IllegalArgumentException("Invalid input");
         }
+        int min = Integer.MIN_VALUE;
+        int secondMin= Integer.MIN_VALUE;
         return secondMin;
     }
 }
