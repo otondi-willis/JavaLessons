556791ced7d3ffd7314e78447aa430e968d3a871
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-19 08:43:44 +0300

    feat: created a class to contain array functions

---
+++ src/main/java/org/sajourney/JavaLessons/dsa/arrays/ArrayFunctions.java
@@ -0,0 +1,10 @@
+package org.sajourney.JavaLessons.dsa.arrays;
+
+public class ArrayFunctions {
+    public static int[] findSecondMin(int[] arr){
+        if(arr==null || arr.length==0){
+
+        }
+        return secondMin;
+    }
+}
